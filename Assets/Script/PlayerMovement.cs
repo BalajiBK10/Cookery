7d64e5085541e641f45d72e16c3af3fe72d0bdad
BalajiBK10 <balakrishan1507@gmail.com>
2025-10-04 20:36:29 +0530

    Update player movement logic

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System;
 using JetBrains.Annotations;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -15,7 +16,44 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetVectorInputNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        
+        float moveDistance =  moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight , playerRadius,moveDir,moveDistance);
+       
+      
+        if (!canMove)
+        {
+            //cannot move towards moveDir
+            //Attempt only on x movement
+             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                // move only on x direction
+                moveDir = moveDirX;
+            }
+
+            else
+            {
+                //cannot move towards moveDir
+                //Attempt to move on z
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                 if (canMove)
+            {
+                //move only on z direction
+                moveDir = moveDirZ;
+            }
+            }
+            
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+       
 
         isWalking = moveDir != Vector3.zero;
 
