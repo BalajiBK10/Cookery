7f027078fdd0fa4d0be0503605bc882b4b8ab870
BalajiBK10 <balakrishan1507@gmail.com>
2025-10-10 09:03:29 +0530

    Initializing the Clear Counter and playerMovement script changes

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,30 +5,62 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
-{
 
+{
+    private ClearCounter clearCounter;
+    [SerializeField] LayerMask counterLayerMask;
     [SerializeField] float moveSpeed = 5f;
+    Vector3 lastInteractions;
 
     bool isWalking;
 
     [SerializeField] private GameInput gameInput;
     void Update()
     {
+        HandleInteraction();
+        HandlePlayerMovement();
+    }
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteraction()
+    {
         Vector2 inputVector = gameInput.GetVectorInputNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        float interactDistance = 2f;
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractions = moveDir;
+        }
+        if (Physics.Raycast(transform.position, lastInteractions, out RaycastHit raycastHit,interactDistance,counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+    
+    private void HandlePlayerMovement()
+    {
+         Vector2 inputVector = gameInput.GetVectorInputNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         
         float moveDistance =  moveSpeed * Time.deltaTime;
         float playerRadius = .7f;    
         float playerHeight = 2f;
         bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight , playerRadius,moveDir,moveDistance);
-       
-      
+
+
         if (!canMove)
         {
             //cannot move towards moveDir
             // if we are trying to move both forwards and right 
             //Attempt only on x movement
-             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
             {
@@ -49,14 +81,14 @@ public class PlayerMovement : MonoBehaviour
                 }
                 else
                 {
-                  // Cannot move on any direction
+                    // Cannot move on any direction
+                }
             }
-            }
-            
+
         }
         if (canMove)
         {
-            transform.position += moveDir * moveDistance;
+            transform.position += moveDir * moveDistance; // moveDistance is the variable for moveSpeed and time.delta time which is declared above the if statement
         }
        
 
@@ -65,10 +97,5 @@ public class PlayerMovement : MonoBehaviour
         transform.rotation = Quaternion.LookRotation(moveDir);
         float rotationSpeed = 10f;
         Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-
-    }
-     public bool IsWalking()
-    {
-        return isWalking;
     }
 }
